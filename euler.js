77a497775881bd66747f8e5a268c0e0e69023607
Brandon Dreadin <brandondreadin@gmail.com>
2018-04-11 19:15:36 -0500

    Added problem 5

--- euler.js
+++ euler.js
@@ -32,11 +32,84 @@ function primeFactors(num){
     var factors = [];
     for(var i = 2; i <= num; i++){
  	   while (isDivisible(num, i)){
-    	   factors.push(i);
+			factors.push(i);
 			num = num / i;
     	}
     }
     return factors;
+}
+
+function isPalindrome(n){
+	var str = n.toString();
+	var reverse = str.split("").reverse().join("");
+	if (str == reverse){
+		return true;
+	}
+	else {return false;}
+}
+
+function primeFactorization (num){
+	//returns array of prime factorization
+	if (num == 1){
+		return [1];
+	}
+	var primes = [];
+	var i = 2;
+
+	while (i <= num){
+		if (isPrime(i) && num%i==0){
+			primes.push(i);
+			num /= i;
+		}
+		else{i++;}
+	}
+	return primes;
+}
+
+
+function numOccurrences(arr, n){
+	arr.sort();
+	var x = arr.indexOf(n);
+	if (x == -1){return 0;}
+	
+	var occurences = 1;
+	for (i = x+1; x <= arr.length -1; i++){
+		if (arr[i] != n){
+			return occurences;
+		}
+		else{occurences++;}
+	}
+	return occurences;
+}
+
+function lcm(arr){
+	//returns least common multiple of numbers in array using prime factorization method
+	var product = 1;
+	var primesArr = [];
+	
+	for (index=0; index <= arr.length - 1; index++){
+		primesArr.push(primeFactorization(arr[index]));
+	}
+
+	var maxArr = Math.max.apply(Math, arr);
+	for (j=2; j <= maxArr; j++){
+		if(!isPrime(j)){continue;}
+
+
+		// finds the highest power for prime j 
+		var highestPower = 0;
+		for (k=0; k <= primesArr.length - 1; k++){
+			var numOcc = numOccurrences(primesArr[k], j);
+			if (numOcc > highestPower){
+				highestPower = numOcc;
+			}
+		}
+
+		if (highestPower){
+			product *= Math.pow(j,highestPower);
+		}
+	}
+	return product;
 }
 
 
@@ -70,4 +143,45 @@ problemSet[2] = function() {
 problemSet[3] = function () {
 	var factors = primeFactors(600851475143);
 	return factors[factors.length - 1];
+}();
+
+
+problemSet[4] = function () {
+	var palindromes = [];
+
+	for (i = 100; i < 1000; i++){
+		for (j = 100; j < 1000; j++){
+			var product = i*j;
+			if (isPalindrome(product)){
+				palindromes.push(product);
+			}
+		}
+	}
+	
+	var max = Math.max.apply(Math, palindromes);
+	return max;
+}();
+
+problemSet[5] = function () {
+	/*
+	//brute force code
+	var i = 2520;
+	var match = false;
+
+	while (true){
+		for (j=20; j > 1; j--){
+			if (i%j){
+				break;
+			}
+			if (j==2){
+				match = true;
+			}
+		}
+
+		if (match){
+			return i;
+		}
+		else {i+=20;}
+	}*/
+	return lcm([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
 }();
